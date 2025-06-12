edc4754aa387904bdd702f32c656002cf1982963
Angel Matos <angelmatos@Angels-MacBook-Air.local>
2025-06-12 10:46:18 -0400

    palindrome finder

---
+++ cc2.py
@@ -0,0 +1,12 @@
+# Palindrome Check
+# Given a string s, return true if s is a palindrome, and false otherwise.
+# Ignore casing and non-alphanumeric characters.
+# Example 1:
+# Input: s = "refer"
+# Output: true
+# Example 2:
+# Input: s = "racecar"
+# Output: true
+# Example 3:
+# Input: s = “gear”
+# Output: false
