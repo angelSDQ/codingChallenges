ac7d6317df5714513dac2c77b54e2e4a50f3594b
Angel Matos <angelmatos@Angels-MacBook-Air.local>
2025-07-06 13:09:59 -0400

    new challenges

---
+++ Java/cc2.java
@@ -0,0 +1,55 @@
+package Java;
+
+// Palindrome Check
+// Given a string s, return true if s is a palindrome, and false otherwise.
+// Ignore casing and non-alphanumeric characters.
+// Example 1:
+// Input: s = "refer"
+// Output: true
+// Example 2:
+// Input: s = "racecar"
+// Output: true
+// Example 3:
+// Input: s = “gear”
+// Output: false
+
+// SOLUTTION
+
+// # DEFINE a function palindrome_checker passing (input_p) parameter
+// # create variable palindrome_word to store the reversed string set to empty ''
+// #   palindrome_word = ''
+// #   use a for loop to iterate the parameter input_p
+// #       use the variable palindrome_word and set to i (the elements from the iterations) + (adding elements to left) palindrome_word
+// #   use if statement to compare if palindrome_word and input_p are the same by using ==
+// #       if true return True
+// #   else
+// #       if false return False
+// #
+// # print and call palindrome_checker and pass word
+
+// def palindrome_checker(input_p):
+//     # return True if input_p == input_p[::-1] else False
+//     palindrome_word = ''
+//     for i in input_p:
+//         palindrome_word = i + palindrome_word
+//     if palindrome_word == input_p:
+//         return True
+//     else:
+//         return False
+
+public class cc2 {
+    public static void main(String[] args) {
+        System.out.println(palindromeChecker("racecar"));
+    }
+    public static boolean palindromeChecker(String inputP) {
+        String palindromeWord = "";
+        for (int i = 0; i < inputP.length(); i++) {
+            palindromeWord = inputP.charAt(i) + palindromeWord;        
+        }
+        if (palindromeWord.toLowerCase().equals(inputP.toLowerCase())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ JavaScript/cc1.js
@@ -0,0 +1,40 @@
+// Reverse String
+// Given a string s, return the string reversed.
+// You must build the reversed string without using the built-in reverse() method.
+// Example 1:
+// Input: s = "hello"
+// Output: "olleh"
+// Example 2:
+// Input: s = "ChatGPT"
+// Output: "TPGtahC"
+
+// #print('hello world')
+
+// DEFINE a function reversed_string passing (input_s) parameter
+// create variable reversed_s to store the reversed string set to empty ''
+//    reversed_s = ''
+//    use a for loop to iterate the parameter input_s
+//        use the variable reversed_s and set to i (the elements from the iterations) + (adding elements to left) reversed_s
+//    return reversed_s
+//
+// print and call function
+
+
+// def reversed_string(input_s):
+//     reversed_s = ''
+//     for i in input_s:
+//         reversed_s = i + reversed_s    
+//     return reversed_s
+
+
+
+function reversed_string(input_s) {
+    let reversed_s = ''
+    for (let i = 0; i < input_s.length; i++) {
+        reversed_s = input_s[i] + reversed_s;
+    }
+    return reversed_s;
+}
+
+console.log(reversed_string('ChatGPT'))
+
